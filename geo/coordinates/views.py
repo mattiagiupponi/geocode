fce75b475447ac3adec5f8ad1e00567df87593d8
Mattia Giupponi <mattia.giupponi@jobrapido.com>
2020-09-18 16:35:20 +0200

    Add test coverages

--- geo/coordinates/views.py
+++ geo/coordinates/views.py
@@ -5,15 +5,15 @@ from coordinates.utils.queryset import from_coordinates_qs_to_list, save_request
 
 
 def find_coordinates(request):
-    operation = request.GET.get("operation")
-    x = float(request.GET.get("x"))
-    y = float(request.GET.get("y"))
-    points = request.GET.get("points")
+    operation, x, y, points = get_url_parameters(request)
+
     save_request_history(request.get_raw_uri(), x, y, points, operation)
+
     coordinate_to_query = from_coordinates_qs_to_list(Coordinate.objects.all())
+
     original_query = coordinate_to_query.copy()
     indexes = []
-    for point in range(0, int(points)):
+    for point in range(0, points):
         closest_index = getattr(CoordinateObj, f"get_{operation}")(
             coordinate_to_query, (x, y)
         )
@@ -22,3 +22,14 @@ def find_coordinates(request):
 
     response = [original_query[index] for index in indexes]
     return JsonResponse({"result": response})
+
+
+def get_url_parameters(request):
+    try:
+        operation = request.GET.get("operation")
+        x = float(request.GET.get("x"))
+        y = float(request.GET.get("y"))
+        points = int(request.GET.get("points"))
+        return operation, x, y, points
+    except Exception as e:
+        raise ValueError("One or more required parameters are empty")
