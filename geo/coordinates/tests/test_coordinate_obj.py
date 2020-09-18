f0d9ecbc77b0e566487141a8c099906de8625505
Mattia Giupponi <mattia.giupponi@jobrapido.com>
2020-09-18 16:27:12 +0200

    Add test coverages

--- geo/coordinates/tests/test_coordinate_obj.py
+++ geo/coordinates/tests/test_coordinate_obj.py
@@ -8,11 +8,11 @@ class TestCoordinateObj(unittest.TestCase):
         self.dataset = [(1, 1), (2, 2), (3, 3), (4, 4)]
         self.coordinate = (2, 2)
 
-    def test_get_nearest(self):
+    def test_get_nearest_index(self):
         actual = self.sut.get_nearest(self.dataset, self.coordinate)
         self.assertEqual(actual, 1)
 
-    def test_get_furthest(self):
+    def test_get_furthest_index(self):
         actual = self.sut.get_furthest(self.dataset, self.coordinate)
         self.assertEqual(actual, 3)
 
